7b66555f1fe3ea6e1b893b1d47096849c18f1dec
Joseph Spinosa <spinosajoseph@gmail.com>
2023-07-06 14:22:48 -0400

    undo lobby start mistake

--- DeadByTwilight/src/screens/LobbyScreen.tsx
+++ DeadByTwilight/src/screens/LobbyScreen.tsx
@@ -205,7 +205,7 @@ export const LobbyScreen: React.FC<
                 width: 200,
               }}>
               <Button
-                disabled={!gameReady && false}
+                disabled={!gameReady}
                 onPress={onStartGame}
                 color="#fff"
                 title="Start Game"
