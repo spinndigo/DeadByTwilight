18da07acacf2fb74e8913ff2699136d43a296b78
Joseph Spinosa <spinosajoseph@gmail.com>
2023-07-11 18:42:27 -0400

    add random hit offset

--- DeadByTwilight/src/components/SkillCheck.tsx
+++ DeadByTwilight/src/components/SkillCheck.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
 import {SegmentedCircle} from '../assests/SegmentedCircle';
+import {getRandomInt} from '../utils/helpers';
 
 export interface CheckProps {
   onGood(): void;
@@ -9,10 +10,22 @@ export interface CheckProps {
 }
 
 // assume repair/heal button is held if this is rendering
+
+const hitOffset = getRandomInt(0, 360);
 export const SkillCheck: React.FC<CheckProps> = props => {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShow(s => !s);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!show) return null;
+
   return (
     <View>
-      <SegmentedCircle {...props} />
+      <SegmentedCircle hitOffset={hitOffset} {...props} />
     </View>
   );
 };
